b0bab0ced02c0c38375a0b6536a57f8850c784fb
Shimul Sattit <shimul@prottoyacademy.com>
2026-05-24 20:35:41 +0600

    Fix route(logout) not defined error in portal-premium layout

--- resources/views/layouts/portal-premium.blade.php
+++ resources/views/layouts/portal-premium.blade.php
@@ -212,7 +212,7 @@
                     <span>লগআউট করুন</span>
                 </div>
             </div>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            <form id="logout-form" action="{{ route('portal.logout') }}" method="POST" style="display: none;">
                 @csrf
             </form>
         </div>
