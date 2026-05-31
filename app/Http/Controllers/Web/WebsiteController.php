e8810b8e6a784b2c6b0f6d0772bb5e6bda5c6de7
Shimul Sattit <shimul@prottoyacademy.com>
2026-05-31 20:09:45 +0600

    Auto-redirect spaces in slugs to clean hyphenated SEO URLs to handle user typos dynamically

--- app/Http/Controllers/Web/WebsiteController.php
+++ app/Http/Controllers/Web/WebsiteController.php
@@ -247,6 +247,12 @@ class WebsiteController extends Controller
 
     public function fetcher($slug, Request $request, $index = 0)
     {
+        // Redirect if slug has spaces to the correct hyphenated SEO URL
+        if (str_contains($slug, ' ')) {
+            $normalizedSlug = preg_replace('/-+/', '-', str_replace(' ', '-', $slug));
+            return redirect()->route('slug.handle', ['slug' => $normalizedSlug], 301);
+        }
+
         $models = ['Category', 'Blog', 'BlogCategory', 'BlogAuthor', 'BlogTag', 'Page', 'JobCategory'];
 
         if ($index >= count($models)) {
